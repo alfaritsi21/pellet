40839ec12595a71b67d7314ccdcd608dae69cf0a
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-09-30 17:37:49 +0700

    feat: add configuration bootstrap vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+require('dotenv').config()
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(Chartkick.use(Chart))
+
 Vue.config.productionTip = false
 
 new Vue({
