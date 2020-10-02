d4e9ae366528edbc39fba8ce12f5d7be94e2aaca
Faishalrmdhn <faishalramadhan0@gmail.com>
2020-10-02 09:52:17 +0700

    add component of profile, personal info, and change PIN

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import menu from './modules/menu'
+
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
-  }
+    menu
+  },
+  getters: {},
+  plugins: [
+    createPersistedState({
+      paths: ['Auth.user', 'Auth.urlApi'],
+      storage: window.sessionStorage
+    })
+  ]
 })
